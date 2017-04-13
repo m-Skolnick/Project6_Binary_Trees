7e488bbd87036efe9d35c93a01d3a8d1d69242e7
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-04-13 12:46:37 -0500

    Generic commit

--- Project6_Binary_Trees/Variables.h
+++ Project6_Binary_Trees/Variables.h
@@ -1,24 +1,16 @@
 //************************************  PROGRAM IDENTIFICATION  ***************************************
 //*                                                                                                   *
-//*   PROGRAM FILE NAME:  Variables.h               ASSIGNMENT #: 4                                   *
+//*   PROGRAM FILE NAME:  Variables.h               ASSIGNMENT #: 6                                  *
 //*                                                                                                   *
 //*                                                                                                   *
 //*   USER DEFINED                                                                                    *
-//*    MODULES     : push - Insert one item to the front of the stack                                 *
-//*                  pop - Remove and return one item from the front of the stack                     *
-//*                  isEmpty - Check to see whether the stack is empty                                *
-//*                  empty - Clear each of the items from the stack                                   *
+//*    MODULES     : NEED TO DO THIS******                                *
 //*                                                                                                   *
 //*****************************************************************************************************
 #ifndef VARIABLES_H
 #define VARIABLES_H
 #include "TreeClass.h"
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <ctype.h>
-#include <string>
-#include <sstream>
+
 using namespace std;
 
 ifstream dataIN; // Declare ifstream for the file containing data.
